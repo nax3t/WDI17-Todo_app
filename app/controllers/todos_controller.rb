14306a58de8128f8c27204d0fc2a2b40b0220e22
Ian <iantskon@gmail.com>
2015-04-24 17:14:49 -0700

    Add index and create action/ Create index view with form

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,12 @@
 class TodosController < ApplicationController
+	def index
+		@todos = Todo.all
+		@todo = Todo.new
+	end
+
+	def create
+		description = params.require(:todo).permit(:description)
+		Todo.create(description)
+		redirect_to todos_path
+	end
 end
